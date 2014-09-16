36b41fa2e3032968518686072d8e9f274e740cc2
Joshua Savigar <tim.savigar@prism-it.co.uk>
2014-09-16 16:20:24 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2807,6 +2807,43 @@ bool InitBlockIndex() {
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
         assert(block.hashMerkleRoot == uint256("0x780985765486749e4591811e7a998d8883b5a2da5faa10db72d242cf49259735"));
+        // *** Tim added code
+        // If genesis block hash does not match, then generate new genesis hash.
+       if (false && block.GetHash() != hashGenesisBlock)
+       {
+           printf("Searching for genesis block...\n");
+           // This will figure out a valid hash and Nonce if you're
+           // creating a different genesis block:
+           uint256 hashTarget = CBigNum().SetCompact(block.nBits).getuint256();
+           uint256 thash;
+           char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
+
+           loop
+           {
+               scrypt_1024_1_1_256_sp_generic(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
+               if (thash <= hashTarget)
+                   break;
+               if ((block.nNonce & 0xFFF) == 0)
+               {
+                   printf("nonce %08X: hash = %s (target = %s)\n", block.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+               }
+               ++block.nNonce;
+               if (block.nNonce == 0)
+               {
+                   printf("NONCE WRAPPED, incrementing time\n");
+                   ++block.nTime;
+               }
+           }
+           printf("block.nTime = %u \n", block.nTime);
+           printf("block.nNonce = %u \n", block.nNonce);
+           printf("block.GetHash = %s\n", block.GetHash().ToString().c_str());
+       }
+
+
+        block.print();
+        
+        // end of added code
+        
         block.print();
         assert(hash == hashGenesisBlock);
         // Start new block file
