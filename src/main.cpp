77b9f79032c89bbebb866544467580e9da3be51f
Joshua Savigar <tim.savigar@prism-it.co.uk>
2014-09-19 10:44:58 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2793,7 +2793,7 @@ bool InitBlockIndex() {
         block.nVersion = 1;
         block.nTime    = 1411116882;
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 294466;
+        block.nNonce   = 0;
 
         if (fTestNet)
         {
